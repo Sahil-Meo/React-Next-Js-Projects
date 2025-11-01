aa103a5be9d9271f3e38fc0da416d0345b7e5bae
Sahil Meo <contact.sahilmeo@gmail.com>
2025-11-01 23:12:42 +0500

    feat: initialize Node.js project with basic setup and add Redux for state management

---
+++ Node JS/sync&async/index.js
@@ -0,0 +1,6 @@
+console.log('hello world, there sahil start this seriease now')
+
+const fs  = require('fs');
+const os = require('os')
+
+console.log(os.cpus().length)
--- react redux/src/LandingPage.jsx
+++ react redux/src/LandingPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 function LandingPage() {
+  const count = useSelector()
   return (
-    <div className='h-[60vh]'>
-      <h1 className="text-4xl text-purple-600 text-center">landing page</h1>
+    <div className='h-[60vh] flex items-center justify-center'>
+      <h1 className="text-4xl text-purple-600 text-center">Learning React Redux Toolkit</h1>
+      <h3 className="text-2xl">Count 0</h3>
     </div>
   )
 }
