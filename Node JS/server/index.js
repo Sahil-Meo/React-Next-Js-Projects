2be63cd2f18683a5e359be54610212bb99a72bdd
Sahil Meo <contact.sahilmeo@gmail.com>
2025-11-02 21:06:26 +0500

    feat: enhance server functionality with improved request logging and add package-lock.json

--- Node JS/server/index.js
+++ Node JS/server/index.js
@@ -1,24 +1,27 @@
 const http = require('http')
 const fs = require('fs')
+const url = require('url')
 
 const myServer = http.createServer((req, res) => {
      console.log('New req send by sahil meo');
-     const log = `${Date.now()} : ${req.url}  new req received\n`
+     const myURL = url.parse(req.url, true)
+     console.log(myURL)
+     const log = `${Date.now()} : ${req.method} ${req.url}  new req received\n`
      fs.appendFile("log.txt", log, (err) => {
           if (err) {
                console.error('Error writing to log file:', err);
                res.end("Internal Server Error");
                return;
           }
-          
+
           switch (req.url) {
-               case '/': 
+               case '/':
                     res.end("You are on home page Right now");
                     break;
-               case '/about': 
+               case '/about':
                     res.end("Hey it's me Sahil Meo");
                     break;
-               default: 
+               default:
                     res.end("Nothing is available yet on this path");
                     break;
           }
