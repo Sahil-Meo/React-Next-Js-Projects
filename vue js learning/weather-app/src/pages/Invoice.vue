42e791fa70da8a9d91adad5cc1c4189570d4bf0d
Sahil Meo <contact.sahilmeo@gmail.com>
2025-12-04 22:15:17 +0500

    feat: add initial Invoice component with template structure and styling

--- vue js learning/weather-app/src/pages/Invoice.vue
+++ vue js learning/weather-app/src/pages/Invoice.vue
@@ -1,1 +1,7 @@
-<template></template>
+<template>
+     <div>
+          <h1 class="text-4xl font-bold text-center">Invoice sender just have in pending</h1>
+     </div>
+</template>
+<script></script>
+<style></style>
